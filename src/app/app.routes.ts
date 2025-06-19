7317407f1f1576bb7c75790aead5c26e5affd5db
Zaca <zacamartines5@gmail.com>
2025-06-19 20:13:46 -0300

    revisar cambios finales al front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,10 @@ import { AdmRestaurant } from './pages/adm-restaurant/adm-restaurant';
 import { AdmMenu } from './pages/adm-menu/adm-menu';
 import { AdmProduct } from './pages/adm-product/adm-product';
 import { Restaurants } from './pages/restaurants/restaurants';
-import { Products } from './pages/products/products';
 import { Menus } from './pages/menus/menus';
-import { EditProduct } from './pages/edit-product/edit-product';
 import { EditRestaurant } from './pages/edit-restaurant/edit-restaurant';
-import { EditMenu } from './pages/edit-menu/edit-menu';
 import { AddRestaurant } from './pages/add-restaurant/add-restaurant'; 
-import { Register } from './register/register'; 
+import { Register } from './pages/register/register'; 
 
 export const routes: Routes = [
   {
@@ -30,11 +27,8 @@ export const routes: Routes = [
   { path: 'adm-product', component: AdmProduct },
   { path: 'adm-menu', component: AdmMenu },
   { path: 'restaurants', component: Restaurants },
-  { path: 'products', component: Products },
   { path: 'menus', component: Menus },
-  { path: 'edit-product', component: EditProduct },
   { path: 'edit-restaurant', component: EditRestaurant },
-  { path: 'edit-menu', component: EditMenu },
   {path: 'add-restaurant', component: AddRestaurant},
   {path: 'register', component: Register}
 
